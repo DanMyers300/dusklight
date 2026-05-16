aead26448d5e12e37e941586e08ece1e9bbf0ff9
Dan <contact@danmyers.net>
2026-05-16 10:23:19 -0500

    Added desktop entry

--- flake.nix
+++ flake.nix
@@ -12,13 +12,23 @@
 
       pkgs = import nixpkgs { inherit system; };
 
+      src = pkgs.fetchurl {
+        url = "https://github.com/TwilitRealm/dusklight/releases/download/v${version}/Dusklight-v${version}-linux-x86_64.AppImage";
+        hash = "sha256-8BtK1XZGHxw2EbbgymV86gvnYPYO6fiZYQdueRs8oWM=";
+      };
+
+      extracted = pkgs.appimageTools.extract {
+        pname = "dusklight";
+        inherit version src;
+      };
+
       dusklight = pkgs.appimageTools.wrapType2 {
         pname = "dusklight";
-        inherit version;
-        src = pkgs.fetchurl {
-          url = "https://github.com/TwilitRealm/dusklight/releases/download/v${version}/Dusklight-v${version}-linux-x86_64.AppImage";
-          hash = "sha256-8BtK1XZGHxw2EbbgymV86gvnYPYO6fiZYQdueRs8oWM=";
-        };
+        inherit version src;
+        extraInstallCommands = ''
+          install -Dm644 ${extracted}/dusklight.desktop $out/share/applications/dusklight.desktop
+          cp -r ${extracted}/usr/share/icons $out/share/icons
+        '';
       };
     in
     {
