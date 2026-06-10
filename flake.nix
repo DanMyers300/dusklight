b2e4d5b64a32493452f0df5e734f2839a7f4783a
Dan <contact@danmyers.net>
2026-06-10 17:25:07 -0500

    updated to 1.3.1

--- flake.nix
+++ flake.nix
@@ -8,13 +8,13 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      version = "1.2.0";
+      version = "1.3.1";
 
       pkgs = import nixpkgs { inherit system; };
 
       src = pkgs.fetchurl {
         url = "https://github.com/TwilitRealm/dusklight/releases/download/v${version}/Dusklight-v${version}-linux-x86_64.AppImage";
-        hash = "sha256-CChoFnLwy6VCGPJtktBvwN1+PpTiSmQ1ka2Wycd2NX4=";
+        hash = "sha256-RuNtxQ+24W3ZVT/1v6Z5Qq3peZSAelLu+8hEObmuIo8=";
       };
 
       extracted = pkgs.appimageTools.extract {
@@ -26,7 +26,7 @@
         pname = "dusklight";
         inherit version src;
         extraInstallCommands = ''
-          install -Dm644 ${extracted}/dusklight.desktop $out/share/applications/dusklight.desktop
+          install -Dm644 ${extracted}/dev.twilitrealm.dusk.desktop $out/share/applications/dusklight.desktop
           cp -r ${extracted}/usr/share/icons $out/share/icons
         '';
       };
